80e4acbba5cd17432e89d545f07cb2233541132c
Moro <hound90@mail.ru>
2025-12-16 16:54:57 +0500

    feat: Добавил unit-test на hooks и reducers с покрытием выше 70%.

--- src/widgets/Header/svg/LogoutSvg.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { LogOutSvg } from "./LogoutSvg";
-
-describe("LogOutSvg", () => {
-  it("должен рендериться", () => {
-    const { container } = render(<LogOutSvg />);
-    expect(container.firstChild).toBeInTheDocument();
-  });
-});
